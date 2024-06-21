2b0468d77aa68706af6de666da41a5e45dce5b87
abhiram406 <118666232+abhiram406@users.noreply.github.com>
2024-06-21 14:26:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ def app():
         result = model.predict(opencv_image)
         for r in result:
             
-            #res_plotted = r[0].plot()
+            res_plotted = r[0].plot()
             #res_plotted = r.plot()[:, :, ::-1]
-            st.write(r[0])
+            #st.write(r[0])
             st.image(res_plotted,
                     caption="Uploaded Image",
                     use_column_width=True
