635d132088229f586f5525ba0d89139607e40890
abhiram406 <118666232+abhiram406@users.noreply.github.com>
2024-06-26 21:35:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,28 +5,23 @@ import numpy as np
 from ultralytics import YOLO
 
 def app():
-    st.header('Object Detection Web App')
-    st.subheader('Powered by YOLOv8')
+    st.header('Automatic Number Plate Detection')
+    st.subheader('AIML Capstone Project - Group 3')
+    #st.subheader('Powered by YOLOv8')
     st.write('Welcome!')
 
     uploaded_file = st.file_uploader(label="Choose an image file",
                  type=['png', 'jpg', 'jpeg'])
     
-    #image = PIL.Image.open(uploaded_file)
-
-    
-    
-
 
     if uploaded_file:
         file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
         opencv_image = cv2.imdecode(file_bytes, 1)
         
-        
-    
         model = YOLO('best.pt')
 
         result = model.predict(opencv_image)
+        st.write(result)
         boxes = result[0].boxes.xyxy.tolist()
         for r in result:
             
@@ -40,12 +35,8 @@ def app():
             norm_img = np.zeros((img.shape[0], img.shape[1]))
             img = cv2.normalize(img, norm_img, 0, 255, cv2.NORM_MINMAX)
             img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)[1]
-            #img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-            
-
             
             res_plotted = r.plot()[:, :, ::-1]
-            #st.write(x1)
             st.image(img,caption="Cropped Image",use_column_width=True)
 
             reader = easyocr.Reader(['en'])
@@ -56,7 +47,6 @@ def app():
             # Print the extracted text
             for detection in result:
                 st.write(detection[1])
-
 
 
 if __name__ == "__main__":
