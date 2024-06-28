ca12f34a7cf1aa0dc542d161de32f7e9b957c29e
abhiram406 <118666232+abhiram406@users.noreply.github.com>
2024-06-29 02:07:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def application():
         opencv_image = cv2.imdecode(file_bytes, 1)
         opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
 
-        sr = dnn_superres.DnnSuperResImpl_create()
+        sr = dnn_superres.DnnSuperResImpl.create()
         path = 'EDSR_x4.pb'
         sr.readModel(path)
         sr.setModel('edsr', 4)
